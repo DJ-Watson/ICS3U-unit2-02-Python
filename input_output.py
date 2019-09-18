bd19d7eb74283743fcb88d292490273a1d07facc
Ubuntu <ubuntu@ip-172-31-28-83.ec2.internal>
2019-09-18 21:36:09 +0000

    finished unit 3-02 python code

---
+++ input_output.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Created by: DJ Watson
+# Created on: September 2019
+# This program calculates area and perimeter of a rectangle
+# with user input
+
+
+def main():
+    # calculates area and perimeter
+    # input code
+    length = int(input("Enter length of the rectangle (mm): "))
+    width = int(input("Enter width of the rectangle (mm): "))
+    # process
+    area = length*width
+    perimeter = 2*(length+width)
+    # output code
+    print("")
+    print("Area is {}mm2".format(area))
+    print("Perimeter is {}mm".format(perimeter))
+
+
+if __name__ == "__main__":
+    main()
